b7eb8a60fc39de7fb1223883735fed29f8106b3c
Miguel Amaya <72326132+miguelxDd@users.noreply.github.com>
2026-04-07 08:34:56 -0600

    make: initialize Spring Boot project structure

---
+++ src/main/java/com/prueba_cuscatlan/shopping_Car_miguel/ShoppingCarMiguelApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_cuscatlan.shopping_Car_miguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCarMiguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCarMiguelApplication.class, args);
+	}
+
+}
